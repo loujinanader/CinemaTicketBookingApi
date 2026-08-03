777628bdf2a7dc28f921f198c3ff177e22eb3fcb
Loujina Nader <loujinanader11@gmail.com>
2026-08-03 17:47:36 +0300

    Refactor BookingService: DI & validation enhancements
    - Introduced dependency injection for IBookingRepository and IMapper in BookingService.
    - Made BookingService a partial class to enable logic separation.
    - Added BookingService.Validation.cs with ValidateBeforeBooking method.
    - Updated CreateBooking to validate input before proceeding.

---
+++ CinemaTicketBookingApi/Services/Bookings/BookingService.Validation.cs
@@ -0,0 +1,14 @@
+﻿using CinemaTicketBookingApi.DTOs.Booking;
+using CinemaTicketBookingApi.Exceptions;
+
+namespace CinemaTicketBookingApi.Services.Bookings
+{
+    public  partial class BookingService
+    {
+        private void ValidateBeforeBooking(CreateBookingDTO booking)
+        {
+            if (booking == null)
+                throw new ArgumentNullException(nameof(booking));
+        }
+    }
+}
--- CinemaTicketBookingApi/Services/Bookings/BookingService.cs
+++ CinemaTicketBookingApi/Services/Bookings/BookingService.cs
@@ -1,13 +1,24 @@
-﻿using CinemaTicketBookingApi.DTOs.Booking;
+﻿using CinemaTicketBookingApi.Data.Mappers;
+using CinemaTicketBookingApi.DTOs.Booking;
 using CinemaTicketBookingApi.Models;
+using CinemaTicketBookingApi.Repository.BookingRepo;
+using CinemaTicketBookingApi.Repository.MovieRepo;
 
 namespace CinemaTicketBookingApi.Services.Bookings
 {
-    public class BookingService : IBookingService
+    public partial class BookingService : IBookingService
     {
+        private readonly IBookingRepository _repository;
+        private readonly IMapper _mapper;
+        public BookingService(IBookingRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
         public Booking CreateBooking(CreateBookingDTO booking)
         {
-            throw new NotImplementedException();
+            ValidateBeforeBooking(booking);
+           throw new NotImplementedException();
         }
         public void CancelBooking(int id)
         {
