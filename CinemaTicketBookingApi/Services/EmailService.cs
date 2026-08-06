9b55a2de643807dea496c95fbd68a2037b8ee082
Loujina Nader <loujinanader11@gmail.com>
2026-08-06 18:25:50 +0300

    Refactor EmailService to use config for credentials
    - Update .gitignore to exclude FodyWeavers.xsd and appsettings.json files in CinemaTicketBookingApi.
    - Refactor EmailService to inject IConfiguration and read email credentials from configuration.
    - Move sender email and app password to EmailSettings section in appsettings.json.
    - Remove hardcoded credentials from code.

--- CinemaTicketBookingApi/Services/EmailService.cs
+++ CinemaTicketBookingApi/Services/EmailService.cs
@@ -4,11 +4,21 @@ namespace CinemaTicketBookingApi.Services
 { 
     public class EmailService
     {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void SendEmail(string to, string subject, string body)
         {
+
+            var emailAddress = _configuration["EmailSettings:Email"];
+            var appPassword = _configuration["EmailSettings:AppPassword"];
             var email = new MimeMessage();
 
-            email.From.Add(MailboxAddress.Parse("loujinanader11@gmail.com"));
+            email.From.Add(MailboxAddress.Parse(emailAddress));
             email.To.Add(MailboxAddress.Parse(to));
             email.Subject = subject;
 
@@ -18,10 +28,15 @@ namespace CinemaTicketBookingApi.Services
             };
 
             using var smtp = new SmtpClient();
+
             smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            smtp.Authenticate("loujinanader11@gmail.com", "zczs xufb bqfe aysi\r\n");
+
+            smtp.Authenticate(emailAddress, appPassword);
+
             smtp.Send(email);
+
             smtp.Disconnect(true);
         }
+
     }
 }
