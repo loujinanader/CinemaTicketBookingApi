b6565d5096952f63e6db2feeb93743386160290f
Loujina Nader <loujinanader11@gmail.com>
2026-08-06 10:06:51 +0300

    Add global ExceptionMiddleware for error handling
    Implemented ExceptionMiddleware to catch and handle exceptions globally in CinemaTicketBookingApi. Maps custom exceptions to HTTP status codes and returns JSON error responses. Added necessary using directives.

--- CinemaTicketBookingApi/Middleware/ExceptionMiddleware.cs
+++ CinemaTicketBookingApi/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,64 @@
-﻿namespace CinemaTicketBookingApi.Middleware
+﻿using CinemaTicketBookingApi.Exceptions.booking;
+using CinemaTicketBookingApi.Exceptions.Movies;
+using System.Text.Json;
+namespace CinemaTicketBookingApi.Middleware
 {
     public class ExceptionMiddleware
     {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+        private static async Task HandleException(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+            switch (ex)
+            {
+                case MovieNotFoundException:
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    break;
+                case BookingNotFoundException:
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    break;
+                case MovieAlreadyExistsException:
+                    context.Response.StatusCode = StatusCodes.Status409Conflict;
+                    break;
+                case MovieNotAvailableException:
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    break;
+                case InsufficientSeatsException:
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    break;
+                case ArgumentNullException:
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    break;
+                case ArgumentException:
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    break;
+                default:
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    break;
+            }
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = ex.Message
+            };
+
+            await context.Response.WriteAsync(
+                JsonSerializer.Serialize(response));
+        }
     }
 }
